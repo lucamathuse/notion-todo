7191ba4b891088d45e453c80b185b267f7567146
Luca Mathuse <mathuse.luca@icloud.com>
2025-12-11 05:50:01 +0100

    Started with design; Sketching out functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// CREATE
+function createNewTask(){
+    console.log("Created a new task");
+}
+
+// READ
+function getTasks(){
+
+}
+
+function getDatabase(){
+    
+}
+
+// UPDATE
+function toggleEditMode(){
+    console.log("Toggled edit mode")
+}
+
+function updateTask(){
+
+}
+
+// DELETE
+function deleteTask(){
+
+}
