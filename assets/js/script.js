1bee77ae728c7ac578ff068c4e302863e5b21f26
Luca Mathuse <mathuse.luca@icloud.com>
2026-01-16 01:08:37 +0100

    backup commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // CREATE
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+    'Custom-Header': 'CustomValue'
+}
+
 function createNewTask(){
-    
+    const element = document.getElementById('todo-content');
+    // Create new entry
+
+    // Upload data to Notion
 }
 
 // READ
@@ -9,12 +18,31 @@ function getTasks(){
 }
 
 function getDatabase(){
+    const url = 'https://api.notion.com/v1/databases/2c3ea3ce57a781e6aeabda73a05db566';
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
 
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error(err));
 }
 
 // UPDATE
 function toggleFilter(){
     
+}
+
+function toggleSettings(){
+    const element = document.getElementById('todo-settings');
+    const button = document.getElementById('settings');
+    if(element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+        button.classList.add('active');
+        
+    } else {
+        element.classList.add('hidden')
+        button.classList.remove('active');
+    }
 }
 
 function updateTask(){
@@ -25,3 +53,11 @@ function updateTask(){
 function deleteTask(){
 
 }
+
+document.addEventListener("DOMContentLoaded", function(e){
+    console.log("Extension has loaded")
+
+    document.getElementById('settings').addEventListener('click', toggleSettings);
+    document.getElementById('add-new').addEventListener('click', createNewTask);
+    document.getElementById('database').addEventListener('click', getDatabase)
+})
